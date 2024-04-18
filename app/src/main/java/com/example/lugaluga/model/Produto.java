e65e74f8164d0fc4f295810dd522a7d8f818e486
Maria Fernanda <mariafernandagomes769@gmail.com>
2024-04-18 16:51:33 -0300

    aula dia 18/04

---
+++ app/src/main/java/com/example/lugaluga/model/Produto.java
@@ -0,0 +1,19 @@
+package com.example.lugaluga.model;
+
+public class Produto {
+
+    private String nome;
+    private Double preco;
+    private String descricao;
+    private int quantidade;
+    private String status;
+
+    public Produto(){
+    }
+
+    public Produto(String nome, Double preco, String descricao,{
+        this.nome = nome;
+        this.preco = preco;
+        this.descricao = descricao;
+    }
+}
