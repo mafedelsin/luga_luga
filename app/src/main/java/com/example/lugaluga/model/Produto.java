7639608947e1ac8c99632149adc150299ad11e9c
Maria Fernanda <mariafernandagomes769@gmail.com>
2024-04-30 16:50:25 -0300

    aula dia 30/04

--- app/src/main/java/com/example/lugaluga/model/Produto.java
+++ app/src/main/java/com/example/lugaluga/model/Produto.java
@@ -5,15 +5,59 @@ public class Produto {
     private String nome;
     private Double preco;
     private String descricao;
-    private int quantidade;
+    private Integer quantidade;
     private String status;
+
+
+    public Produto(String nome, Double preco, String descricao, Integer quantidade, String status) {
+        this.nome = nome;
+        this.preco = preco;
+        this.descricao = descricao;
+        this.quantidade = quantidade;
+        this.status = status;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 
     public Produto(){
     }
 
-    public Produto(String nome, Double preco, String descricao,{
-        this.nome = nome;
-        this.preco = preco;
-        this.descricao = descricao;
-    }
 }
--- app/src/main/java/com/example/lugaluga/view/MainActivity.java
+++ app/src/main/java/com/example/lugaluga/view/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.lugaluga.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.View;
@@ -10,38 +11,38 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 import com.example.lugaluga.R;
+import com.example.lugaluga.model.Produto;
+import com.example.lugaluga.view.adapter.AdapterProduto;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ListView listCidade;
-    private String[] cidades = {
-            "São Carlos", "Araraquara", "Ibaté", "Ribeirão Bonito", "Dourado", "Descalvado", "Porto Ferreira",
-            "Santa Rita Do Passa Quatro", "Tambaú", "Ribeirão Preto", "Jaboticabal"
-    };
+    private RecyclerView recyclerView;
 
+    private AdapterProduto adapterProduto;
+
+    private List<Produto> produtoList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        listCidade = findViewById(R.id.listaNomes);
+        recyclerView.findViewById(R.id.Produtos);
+    }
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(
-                getApplicationContext(),
-                android.R.layout.simple_list_item_1,
-                android.R.id.text1,
-                cidades
-        );
-
-        listCidade.setAdapter(adapter);
-
-        listCidade.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                String itemSelecionado = listCidade.getItemAtPosition(position).toString();
-                Toast.makeText(getApplicationContext(), itemSelecionado, Toast.LENGTH_LONG).show();
-            }
-        });
+    public void CriarListaProdutos(){
+        Produto produto =
+                new Produto(
+                        "IPhone 15",
+                        200.00,
+                        "Descrição",
+                        10,
+                        "Disponível"
+                );
+        produtoList.add(produto);
 
     }
+
+
 }
