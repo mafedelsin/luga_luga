8c3f3ea0b834e866dfcfe7537e54edacf8c7cbc9
Maria Fernanda <mariafernandagomes769@gmail.com>
2024-05-02 14:28:13 -0300

    aula dia 2/05

--- app/src/main/java/com/example/lugaluga/view/MainActivity.java
+++ app/src/main/java/com/example/lugaluga/view/MainActivity.java
@@ -1,19 +1,24 @@
 package com.example.lugaluga.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import com.example.lugaluga.R;
+import com.example.lugaluga.RecyclerItemClickListener;
 import com.example.lugaluga.model.Produto;
 import com.example.lugaluga.view.adapter.AdapterProduto;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,26 +27,136 @@ public class MainActivity extends AppCompatActivity {
 
     private AdapterProduto adapterProduto;
 
-    private List<Produto> produtoList;
+    private List<Produto> produtoList = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        recyclerView.findViewById(R.id.Produtos);
+        recyclerView = findViewById(R.id.Produtos);
+
+        CriarListaProdutos();
+
+        adapterProduto = new AdapterProduto(produtoList);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        recyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayout.VERTICAL));
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setAdapter(adapterProduto);
+
+
+        recyclerView.addOnItemTouchListener(new RecyclerItemClickListener(getApplicationContext(), recyclerView, new RecyclerItemClickListener.OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+
+            }
+
+            @Override
+            public void onLongItemClick(View view, int position) {
+
+            }
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+        }));
+
     }
 
     public void CriarListaProdutos(){
-        Produto produto =
-                new Produto(
+        Produto produto;
+        produto = new Produto(
                         "IPhone 15",
-                        200.00,
+                        8000.00,
                         "Descrição",
                         10,
                         "Disponível"
                 );
         produtoList.add(produto);
 
+
+        produto = new Produto(
+                        "Fone",
+                        50.00,
+                        "Bluetooth",
+                        105,
+                        "Disponível"
+                );
+        produtoList.add(produto);
+
+        produto = new Produto(
+                        "IPhone 11",
+                        2000.00,
+                        "Descrição",
+                        41,
+                        "Disponível"
+                );
+        produtoList.add(produto);
+
+        produto = new Produto(
+                        "Ar condicionado",
+                        1500.00,
+                        "Descrição",
+                        20,
+                        "Disponível"
+                );
+        produtoList.add(produto);
+
+        produto = new Produto(
+                        "IPhone 12",
+                        3000.00,
+                        "Descrição",
+                        60,
+                        "Disponível"
+                );
+        produtoList.add(produto);
+
+        produto = new Produto(
+                        "IPhone 13",
+                        4000.00,
+                        "Descrição",
+                        89,
+                        "Disponível"
+                );
+        produtoList.add(produto);
+
+        produto = new Produto(
+                        "Iphone XR",
+                        1000.00,
+                        "Descrição",
+                        58,
+                        "Disponível"
+                );
+        produtoList.add(produto);
+
+        produto = new Produto(
+                        "IPhone 14",
+                        3000.00,
+                        "Descrição",
+                        35,
+                        "Disponível"
+                );
+        produtoList.add(produto);
+
+        produto = new Produto(
+                        "IPhone 11 pro",
+                        1500.00,
+                        "Descrição",
+                        31,
+                        "Disponível"
+                );
+        produtoList.add(produto);
+
+        produto =
+                new Produto(
+                        "iPad",
+                        1000.00,
+                        "Descrição",
+                        50,
+                        "Disponível"
+                );
+        produtoList.add(produto);
     }
 
 
